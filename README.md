2d78d99599fbe9d9f6ac8338d7b042ad52cb91f0
Mr. Glass <mrglass@gmail.com>
2021-05-16 03:31:06 -0400

    POC

---
+++ README.md
@@ -0,0 +1,8 @@
+Discord Snitch, v1
+
+Dockerfile & VSCode devcontainer included
+
+Devcontianer usage:
+   Copy `.devcontainer/devcontainer.env.dist` to `.devcontainer/devcontainer.env.dist`
+   Fill in the approiate settings in the env file
+   Run app.py (F5)
---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+
+import discord
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+BANNED_WORDS = [
+    'shit',
+    'piss',
+    'fuck',
+    'cunt',
+    'cocksucker',
+    'motherfucker',
+    'tits',
+]
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+@client.event
+async def on_message(message):
+    if message.author.id != client.user.id:
+        matches = {word for word in BANNED_WORDS if word in message.content}
+        if matches:
+            alert = f'{message.author.name}: {message.content} with includes the words {matches} {message.jump_url}'
+            chan = discord.utils.get(message.guild.channels, name=os.getenv('ALERT_CHANNEL'))
+            await chan.send(alert)
+
+client.run(TOKEN)
